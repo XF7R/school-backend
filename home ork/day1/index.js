407fdcf6dd295b19fd77500dc2e93fea0d86a52a
battal <almtyrybtal1@gmail.com>
2023-08-10 16:59:28 +0300

    day2

---
+++ home ork/day1/index.js
@@ -0,0 +1,9 @@
+let age =16;
+
+let name ="battal_saad";
+
+console.log(age);
+console.log(name);
+
+// (;  2023@ جميع الحقوق محفوضة عند ( بتال المطيري ) 
+
---
+++ home ork/day2/index.js
@@ -0,0 +1,12 @@
+let number =42;
+let numbrTostring =String(number);
+console.log(`${typeof number} : ${number}`);
+console.log(`${typeof numbrTostring} : ${numbrTostring}`);
+
+let String ="25";
+let stringTonumber=number(String);
+console.log(`${typeof String} : ${String}`);
+console.log(`${typeof stringTonumber} : ${stringTonumber}`);
+
+
+//   نسيت احله  (:
