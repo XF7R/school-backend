47632632a25f5ac2df7f572db5cc43831afe3f36
battal <almtyrybtal1@gmail.com>
2023-08-14 03:02:20 +0300

    day3

---
+++ home ork/day3/index.js
@@ -0,0 +1,13 @@
+
+    let Names = ["ali" , "samah" , "battal" ,"fahad" , "ssultan" ]
+
+    console.log(Names);
+    console.log(Names[2]);
+    Names.unshift("chess")
+    console.log(Names);
+    Names.push("grandmaster")
+    console.log(Names);
+    Names.shift()
+    console.log(Names);
+    Names.pop()
+    console.log(Names);
